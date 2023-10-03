0cf9487c9b015bc9ff38cdf6d83a9e295f684835
lia0anahi <141980286+lia0anahi@users.noreply.github.com>
2023-10-02 23:25:20 -0300

    Add files via upload

---
+++ src/workflow-inicial/601_RUN_correr_workflow_vf1.r
@@ -0,0 +1,32 @@
+# Corrida general del workflow
+rm(list = ls()) # Borro todos los objetos
+gc() # Garbage Collection
+
+library("data.table")
+library("yaml")
+library("Rcpp")
+library("ranger")
+library("randomForest") # solo se usa para imputar nulos
+library("R6")
+library("lightgbm")
+
+
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_vf1.r")
+source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_vf1.r")
+source("~/labo2023ba/src/workflow-inicial/631_FE_historia_vf1.r")
+source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_vf1.r")
+
+# ultimos pasos, muy lentos
+source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_vf1.r")
+source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_vf1.r")
